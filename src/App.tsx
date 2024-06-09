c11c465198b6c128551e11453beede1d709ce374
sumaiyarimu22 <sumaiyarimu22@gmail.com>
2024-06-09 16:17:08 +0600

    basis route add & bashboard sidebar add

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import MainLayout from "./components/layout/MainLayout";
+
 const App = () => {
-  return <div>App</div>;
+  return <MainLayout />;
 };
 
 export default App;
---
+++ src/components/layout/MainLayout.tsx
@@ -0,0 +1,83 @@
+import { Layout, Menu, MenuProps } from "antd";
+import { NavLink, Outlet } from "react-router-dom";
+
+const { Header, Content, Footer, Sider } = Layout;
+
+const items: MenuProps["items"] = [
+  {
+    key: "DashBoard",
+    label: <NavLink to='/admin/dashboard'>DashBoard</NavLink>,
+  },
+
+  {
+    key: "User Management",
+    label: "User Management",
+    children: [
+      {
+        key: "Create Admin",
+        label: <NavLink to='/admin/create-admin'>Create Admin</NavLink>,
+      },
+      {
+        key: "Create Faculty",
+        label: <NavLink to='/admin/create-faculty'>Create Faculty</NavLink>,
+      },
+      {
+        key: "Create Student",
+        label: <NavLink to='/admin/create-student'>Create Student</NavLink>,
+      },
+    ],
+  },
+];
+
+const MainLayout: React.FC = () => {
+  return (
+    <Layout style={{ height: "100vh" }}>
+      <Sider
+        breakpoint='lg'
+        collapsedWidth='0'
+        onBreakpoint={(broken) => {
+          console.log(broken);
+        }}
+        onCollapse={(collapsed, type) => {
+          console.log(collapsed, type);
+        }}
+      >
+        <div
+          style={{
+            color: "white",
+            height: "4rem",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <h1>UniManagement</h1>
+        </div>
+        <Menu
+          theme='dark'
+          mode='inline'
+          defaultSelectedKeys={["4"]}
+          items={items}
+        />
+      </Sider>
+      <Layout>
+        <Header style={{ padding: 0 }} />
+        <Content style={{ margin: "24px 16px 0" }}>
+          <div
+            style={{
+              padding: 24,
+              minHeight: 360,
+            }}
+          >
+            <Outlet />
+          </div>
+        </Content>
+        <Footer style={{ textAlign: "center" }}>
+          All Rright uni managemanet {new Date().getFullYear()}
+        </Footer>
+      </Layout>
+    </Layout>
+  );
+};
+
+export default MainLayout;
